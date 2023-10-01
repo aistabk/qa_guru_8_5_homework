f8b5f54433e10a338d5d56c5737ebbd74aa6b8ea
aistabk <elizavetasoom@ya.ru>
2023-10-01 22:16:06 +0300

    внесены исправления

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,6 @@ def browser_config():
     browser.config.base_url = 'https://demoqa.com'
     #browser.config.timeout = 8.0
     browser.config.window_width = 600
-    browser.config.window_height = 1900
+    browser.config.window_height = 900
     yield
     browser.quit()
